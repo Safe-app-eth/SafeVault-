d33d3b4bc8838fe2670fb2a6486bc43ddc56a8e6
Ethersafe.eth <raphaelchigozle7088@icloud.com>
2025-08-15 07:35:59 +0100

    Update tailwind.config.js
    Signed-off-by: Ethersafe.eth <raphaelchigozle7088@icloud.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{ts,tsx}"],
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{ts,tsx,js,jsx}"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
