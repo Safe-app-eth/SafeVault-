eeac9f98f78412a524b0523bc22f4130464aa4ba
thegood.eth <rchigozle60@gmail.com>
2025-08-16 01:57:25 +0100

    Update signerVerification.ts
    Signed-off-by: thegood.eth <rchigozle60@gmail.com>

--- src/utils/signerVerification.ts
+++ src/utils/signerVerification.ts
@@ -1,9 +1,6 @@
 import { ethers } from 'ethers';
 
-export const verifySigner = async (
-  expectedAddress: string,
-  signer: ethers.Signer
-): Promise<boolean> => {
+export const verifySigner = async (expectedAddress: string, signer: ethers.Signer): Promise<boolean> => {
   const address = await signer.getAddress();
   return address.toLowerCase() === expectedAddress.toLowerCase();
 };
