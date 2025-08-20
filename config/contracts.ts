0cb18f50dd54c3d32badfa431b822cd69cb7933f
Ethersafe.eth <raphaelchigozle7088@icloud.com>
2025-08-20 15:40:35 +0100

    Update and rename contracts.ts to contracts.ts
    Signed-off-by: Ethersafe.eth <raphaelchigozle7088@icloud.com>

--- config/contracts.ts
+++ config/contracts.ts
@@ -1,41 +1,21 @@
 export const CONTRACTS = {
   1: {
-    name: "Ethereum Mainnet",
+    name: "Ethereum",
     safeAddress: "0xfecBad5D60725EB6fd10f8936e02fa203fd27E4b",
-    rpcUrl: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
     contracts: {
       SafeVault: {
         address: "0x3E5c63644E683549055b9Be8653de26E0B4CD36E",
-        abi: [
-          {
-            inputs: [{ internalType: "address", name: "_singleton", type: "address" }],
-            stateMutability: "nonpayable",
-            type: "constructor"
-          },
-          {
-            stateMutability: "payable",
-            type: "fallback"
-          },
-          {
-            name: "masterCopy",
-            type: "function",
-            inputs: [],
-            outputs: [{ internalType: "address", name: "", type: "address" }],
-            stateMutability: "view"
-          }
-        ]
+        abi: [/* ABI here */]
       }
     }
   },
-
-  42161: {
-    name: "Arbitrum One",
-    safeAddress: "0xFDf84a0e7D07bC56f7De56696fc409704cC83a24",
-    rpcUrl: `https://arb-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+  8453: {
+    name: "Base",
+    safeAddress: "0x7E9A6b14e78BF18bD483D208af423b96AB5075A4",
     contracts: {
       SafeVault: {
         address: "0x3E5c63644E683549055b9Be8653de26E0B4CD36E",
-        abi: [/* same ABI or chain-specific version */]
+        abi: [/* ABI here */]
       }
     }
   }
