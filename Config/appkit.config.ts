92287dfcd288490b2b64b0caba7e860cf55147b5
Ethersafe.eth <raphaelchigozle7088@icloud.com>
2025-08-24 22:57:16 +0100

    Create appkit.config.ts
    Signed-off-by: Ethersafe.eth <raphaelchigozle7088@icloud.com>

---
+++ Config/appkit.config.ts
@@ -0,0 +1,9 @@
+export const APPKIT_CONFIG = {
+  projectId: "YOUR_PROJECT_ID",
+  rpcUrl: "https://rpc.reown.com",
+  supportedChains: [
+    { id: 1, name: "Ethereum" },
+    { id: 8453, name: "Base" },
+    { id: 42161, name: "Arbitrum" }
+  ]
+};
