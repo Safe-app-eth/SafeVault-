bdd73f564323544df893d9319d188325854ab5f4
thegood.eth <rchigozle60@gmail.com>
2025-08-24 23:31:05 +0100

    Create dashboard.config.ts
    Signed-off-by: thegood.eth <rchigozle60@gmail.com>

---
+++ src/config/dashboard.config.ts
@@ -0,0 +1,14 @@
+export const DASHBOARD_CONFIG = {
+  vaults: [
+    {
+      name: 'Mainnet Treasury',
+      chainId: 1,
+      safeAddress: '0xFDf84a0e7D07bC56f7De56696fc409704cC83a24'
+    },
+    {
+      name: 'Arbitrum Ops Vault',
+      chainId: 42161,
+      safeAddress: '0x9a8FEe232DCF73060Af348a1B62Cdb0a19852d13'
+    }
+  ]
+};
